b7d0f62f31c467d06157055909d5fde0a5b8299d
Usta VN <usta.cyon@gmail.com>
2025-10-25 07:50:43 +0000

    Deploy from web-app@develop 2025-10-25 07:50:43

---
+++ assets/index-752072bb.js
@@ -0,0 +1,1 @@
+import{j as e,P as t}from"./index-9951c232.js";const r=()=>e.jsx(t,{title:"Starter Page",subName:"Pages"});export{r as default};
--- assets/index-7f11dd33.js
+++
@@ -1,1 +0,0 @@
-import{j as e,P as t}from"./index-d6599812.js";const r=()=>e.jsx(t,{title:"Starter Page",subName:"Pages"});export{r as default};
