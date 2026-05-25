c1149c69bbad2608b9ebc2b58ed6e9d97b069850
Usta VN <usta.cyon@gmail.com>
2026-05-25 10:07:26 +0000

    Deploy from web-app@develop 2026-05-25 10:07:26

--- assets/NavbarContext-1352985c.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./index-f9639d58.js";const t=a.createContext(null);t.displayName="NavbarContext";const e=t;export{e as N};
--- assets/documentClient-418ccc78.js
+++
@@ -1,1 +0,0 @@
-import{a7 as e,a8 as n}from"./index-f9639d58.js";async function r(t){const a=new FormData;a.append("file",t);const o=await e.post("/documents/upload",a,{headers:{"Content-Type":"multipart/form-data"}});return n(o)}export{r as u};
